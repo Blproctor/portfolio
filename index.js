7cc66bde4a036c6a6fc37142a7a21332c71a5e6e
Blproctor <Brittany.l.proctor@gmail.com>
2017-09-27 21:40:54 -0400

    Initial commit
    This is the first commit for my portfolio.  It utilizes the Browser
    Template from GA and the Freelancer Bootstrap template from
    startbootstrap.com.

---
+++ index.js
@@ -0,0 +1,34 @@
+'use strict'
+
+// user require with a reference to bundle the file and use it in this file
+// var example = require('./example');
+
+// load manifests
+// scripts
+require('./assets/scripts/index.js')
+require('./assets/scripts/js/contact_me.js')
+require('./assets/scripts/js/freelancer.js')
+require('./assets/scripts/js/freelancer.min.js')
+require('./assets/scripts/js/jqBootstrapValidation.js')
+
+// styles
+// require('./assets/styles/index.scss')
+require('./assets/styles/css/freelancer.css')
+require('./assets/styles/css/freelancer.min.css')
+require('./assets/styles/scss/_bootstrap-overrides.scss')
+require('./assets/styles/scss/_contact.scss')
+require('./assets/styles/scss/_global.scss')
+require('./assets/styles/scss/_masthead.scss')
+require('./assets/styles/scss/_mixins.scss')
+require('./assets/styles/scss/_navbar.scss')
+require('./assets/styles/scss/_portfolio.scss')
+require('./assets/styles/scss/_variables.scss')
+require('./assets/styles/scss/freelancer.scss')
+require('./assets/vendor/bootstrap/css/bootstrap-grid.css')
+require('./assets/vendor/bootstrap/css/bootstrap-grid.min.css')
+require('./assets/vendor/bootstrap/css/bootstrap-reboot.css')
+require('./assets/vendor/bootstrap/css/bootstrap-reboot.min.css')
+require('./assets/vendor/bootstrap/css/bootstrap.css')
+require('./assets/vendor/bootstrap/css/bootstrap.min.css')
+require('./assets/vendor/bootstrap/js/bootstrap.js')
+require('./assets/vendor/bootstrap/js/bootstrap.min.js')
